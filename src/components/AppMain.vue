9a78daad3f8a44f82efaabc387b96309d83631c6
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-06-07 16:14:10 +0200

    app.vue problem

---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+// CHIAMATA AL FILE JS STORE COME STORAGE DI RIFERIMENTO
+import { store } from './store.js'
+import cardMovie from './cardMovie.vue'
+
+export default {
+    name : "AppMain",
+    components: {
+        cardMovie
+    },
+    data() {
+        return{
+        // ACCESSO AI DATI DELLO STORAGE PRINCIPALE
+        store
+        }
+    },
+}
+</script>
+
+<template>
+    <cardMovie/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
